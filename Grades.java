433bef590a6b5a859099e75eb4f6537ef41984d8
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-03-19 14:29:25 -0400

    Create Grades.java

---
+++ Grades.java
@@ -0,0 +1,35 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementFilter;
+public class Grades
+{
+    public static void main (String args [])
+    {
+        int grade;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your grade: ");
+        grade = input.nextInt();
+        input.close();
+        if (grade >= 95)
+        {
+            System.out.println("A");
+        }
+        else if (grade >= 90)
+        {
+            System.out.println("B");
+        }
+        else if (grade >= 85)
+        {
+            System.out.println("C");
+        }
+        else if (grade >= 80)
+        {
+            System.out.println("D");
+        }
+        else
+        {
+            System.out.println("F");
+        }
+    }
+}
